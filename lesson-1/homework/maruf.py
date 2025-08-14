55eaf80436b9772900b55cd90eb77b0d8bed1463
shodievm93collab <shodievm93@gmail.com>
2025-08-14 18:49:54 +0500

    Update maruf.py

--- lesson-1/homework/maruf.py
+++ lesson-1/homework/maruf.py
@@ -1,1 +1,28 @@
+#    Given a side of square. Find its perimeter and area.
 
+a = 4
+b = 6
+perimetr = 4+4+6+6
+area = 6*4
+print("kvadrat perimetri:", perimetr)
+print("kvadrat maydoni:", area)
+
+#    Given two numbers a and b. Find their mean.
+
+a = 8
+b = 12
+print((8+12)/2)
+
+# Given two numbers a and b. Find their sum, product and square of each number.
+
+a = float(input("Enter number a: "))
+b = float(input("Enter number b: "))
+
+sum_ab = a + b
+product_ab = a * b
+square_a = a ** 2
+square_b = b ** 2
+print("Sum of a and b:", sum_ab)
+print("Product of a and b:", product_ab)
+print("Square of a:", square_a)
+print("Square of b:", square_b)
